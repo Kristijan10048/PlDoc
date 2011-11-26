37bdae14ea78d2fb3304d0b45a2097408a466263
sturton <sturton@users.noreply.github.com>
2011-11-26 22:34:07 +0000

    correct Bug reference from 3442994 to 3443147

--- pldoc/trunk/src/test/resources/testinput/bugs/pldoc-3443147.sql
+++ pldoc/trunk/src/test/resources/testinput/bugs/pldoc-3443147.sql
@@ -1,6 +1,6 @@
 
 
-  CREATE OR REPLACE TYPE PLDOC_BUG.TESTCASE_3442994 
+  CREATE OR REPLACE TYPE PLDOC_BUG.TESTCASE_3443147 
   /** This is the simplest inherited subtype - it should still be documented
   *@headcom
   */
