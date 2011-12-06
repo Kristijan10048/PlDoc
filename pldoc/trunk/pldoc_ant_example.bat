e08d25c1b786e55cc93083e1777fabf4c4f04dd0
sturton <sturton@users.noreply.github.com>
2011-12-06 22:49:12 +0000

    Pre 0.9.8 Release commit

--- pldoc/trunk/pldoc_ant_example.bat
+++ pldoc/trunk/pldoc_ant_example.bat
@@ -3,7 +3,12 @@ setlocal
 set PLDOC_HOME=%~dp0
 if "%PLDOC_HOME%" == "" ( echo ERROR: Environment variable PLDOC_HOME not set. 1>&2 && exit /b 1 )
 
-set ANT_OPTS=-server -Dpldoc.home=%PLDOC_HOME%
+@REM The endorsed directories mechanism is necessary because of the exslt:node-set calls in unit.xsl 
+@REM In production, reference the standalone XALAN and XERCES jar files in java.endorsed.dirs, 
+@REM use the standalone PLDoc jar and Oracle JDBC as normal
+
+set INITIAL_ANT_OPTS=%ANT_OPTS%
+set ANT_OPTS=-server -Djava.endorsed.dirs=%PLDOC_HOME%/target -Dpldoc.home=%PLDOC_HOME% %INITIAL_ANT_OPTS%
 call ant -f ant_example.xml
 
 @REM rem Include Oracle jars in the classpath 
@@ -17,7 +22,8 @@ if "%ORACLE_HOME%" == "" ( echo INFO: Environment variable ORACLE_HOME not set. 
 	set NLS_LANG=.UTF8
 	@REM Run against all Oracle Enterprise schemas
 	@REM call ant -DJDBC_HOME=%JDBC_HOME% -f ant_enterprise_example.xml
-	if "%JDBC_HOME%" == "" ( echo "WARN: Cannot locate JDBC directory. Ensure that JDBC_HOME is specified in ant_enterprise_example.properties" 1>&2 && call ant -f ant_enterprise_example.xml ant -propertyfile ant_enterprise_example.properties) else call ant -DJDBC_HOME=%JDBC_HOME% -f ant_enterprise_example.xml -propertyfile ant_enterprise_example.properties
+	@REM It seems that because the PLDoc class is loaded from endorsed dirs, then the JDBC classes loaded in it must also be loaded from endorsed dirs   
+	if "%JDBC_HOME%" == "" ( echo "WARN: Cannot locate JDBC directory. Ensure that JDBC_HOME is specified in ant_enterprise_example.properties and java.endorsed.dirs" 1>&2 && call ant -f ant_enterprise_example.xml ant -propertyfile ant_enterprise_example.properties) else ( set ANT_OPTS=-server -Djava.endorsed.dirs=%PLDOC_HOME%/target;%JDBC_HOME% %INITIAL_ANT_OPTS% && call ant -DJDBC_HOME=%JDBC_HOME% -f ant_enterprise_example.xml -propertyfile ant_enterprise_example.properties)
 	
 
 :end
--- pldoc/trunk/pldoc_example.sh
+++ pldoc/trunk/pldoc_example.sh
@@ -1,2 +1,2 @@
 # $Id$
-./pldoc.sh -doctitle \"Sample Application\" -overview samples/overview1.html -d SampleApplicationDoc samples/sample*.sql
+./pldoc.sh -doctitle \"Sample Application\" -overview samples/overview1.html -d SampleApplicationDoc -inputencoding ISO-8859-15 samples/sample*.sql
