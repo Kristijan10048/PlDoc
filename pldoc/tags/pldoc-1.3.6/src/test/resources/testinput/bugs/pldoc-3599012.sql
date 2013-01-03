2d05841ea5e719e120fca99567fcc19b1ace1176
sturton <sturton@users.noreply.github.com>
2013-01-03 21:17:20 +0000

    [maven-release-plugin]  copy for tag pldoc-1.3.6

---
+++ pldoc/tags/pldoc-1.3.6/src/test/resources/testinput/bugs/pldoc-3599012.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE TYPE pldoc_bug.testcase_3599012 FORCE UNDER wfw_Agenda_t ( 
+  --
+  -- Navigation data - filled in OutNavigatorData
+  Navigation wfw_TNavigation
+  --
+  --
+)
+NOT INSTANTIABLE NOT FINAL;
+/
+
+
