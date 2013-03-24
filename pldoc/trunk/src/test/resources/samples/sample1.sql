79e8252226db7184c9f4dfda4d9f6e13a7e42e7f
sturton <sturton@users.noreply.github.com>
2013-03-24 20:38:34 +0000

    Correct sample1.sql - Parameterless FUNCTION must still return a value

--- pldoc/trunk/src/test/resources/samples/sample1.sql
+++ pldoc/trunk/src/test/resources/samples/sample1.sql
@@ -93,7 +93,7 @@ RETURN NUMBER;
 PROCEDURE Without_Parameters;
 
 /** (no comments) */
-FUNCTION Get_Without_Parameters;
+FUNCTION Get_Without_Parameters RETURN INTEGER;
 
 /* this compiles, but pldoc cannot handle it !
 PROCEDURE function (
