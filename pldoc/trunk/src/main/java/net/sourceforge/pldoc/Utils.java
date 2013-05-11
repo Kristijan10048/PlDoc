9a6f4d601bac701ad671bd71af950d8907c30eea
sturton <sturton@users.noreply.github.com>
2013-05-11 16:48:14 +0000

    PLDOC- Add Javadoc comments to Utils.java; extract methods which create static XSL and CSS files from PLDoc.java into Utils.java for downstream use in other Maven projects (PLDoc.java not yet altered to use the Utils.java methods)

--- pldoc/trunk/src/main/java/net/sourceforge/pldoc/Utils.java
+++ pldoc/trunk/src/main/java/net/sourceforge/pldoc/Utils.java
@@ -30,6 +30,8 @@ public class Utils
 
   /** Copies a file.
   * I wish there was a standard Java way to do it.
+  * @param inputFile source 
+  * @param outputFile target 
   */
   public static void CopyFile(File inputFile, File outputFile)
   throws FileNotFoundException, IOException
@@ -48,6 +50,8 @@ public class Utils
 
   /** Copies a InputStream into a file.
   * I wish there was a standard Java way to do it.
+  * @param inputStream source 
+  * @param outputFile target 
   */
   public static void CopyStreamToFile(InputStream inputStream, File outputFile)
   throws FileNotFoundException, IOException
@@ -66,6 +70,8 @@ public class Utils
 
 
   /** Copies a Reader into a file.
+  * @param reader source 
+  * @param outputFile target 
   */
   public static void CopyReaderToFile(Reader reader, File outputFile)
   throws FileNotFoundException, IOException
@@ -79,6 +85,86 @@ public class Utils
 
     reader.close();
     out.close();
+  }
+
+  /** Aggregate contents of an InputStream into a String 
+  * @param inputStream source 
+  * @return aggregated contents
+  */
+  public static String getStringFromInputStream(InputStream inputStream) throws IOException  
+   {
+	byte[] inputBuffer = new byte[1024];
+	StringBuffer stringBuffer = new StringBuffer(1024);
+
+	    int bytesRead;
+	    while (inputStream.available() > 0) {
+		bytesRead = inputStream.read(inputBuffer);
+		stringBuffer.append(new String(inputBuffer, 0, bytesRead));
+	    }
+        return stringBuffer.toString(); 
+  }
+
+ /**
+  * Copies required static files into the source code directory.
+  *
+  * This allows the XSL and CSS hrefs to cope with relocation of the root output directory 
+  * or access through a web browser.
+  *
+  * @param outputDirectory directory to copy files
+  * @param relativePath the relative path to the location of the root output directory 
+  */
+  public static void copyStaticSourceDirectoryFiles(File outputDirectory, String relativePath) throws Exception {
+    try {
+      // Copy sourcecode.xsl, replacing the stylesheet href with the relative href
+      Properties  replacementProperties = new Properties();
+      replacementProperties.put("sourcestylesheet.css", relativePath + "sourcestylesheet.css");
+      Utils.CopyReaderToFile(
+	new BufferedReader(
+	  new SubstitutionReader( 
+	    new BufferedReader(
+	      new InputStreamReader(
+				    (new ResourceLoader()).getResourceStream("sourcecode.xsl")
+				   )
+		)
+	       ,replacementProperties
+	      )
+	    )
+      , new File(outputDirectory.getPath() + File.separator + "sourcecode.xsl")
+      );
+    } catch(FileNotFoundException e) {
+      System.err.println("File not found. ");
+      e.printStackTrace();
+      throw e;
+    }
+  }
+
+  /**
+  * Copy required static files into the root output directory.
+  *
+  * @param outputDirectory directory to copy files
+  * @param sourceStylesheet the relative path to the location of the root output directory 
+  */
+  public static void copyStaticRootDirectoryFiles(File outputDirectory, File stylesheet, File sourceStylesheet) throws Exception {
+    try {
+      // Copy sourcecode.xsl, replacing the stylesheet href with the relative href
+      Utils.CopyStreamToFile(
+	  (null != stylesheet && stylesheet.exists()) 
+	  ? new FileInputStream ( stylesheet ) 
+	  : (new ResourceLoader()).getResourceStream("defaultstylesheet.css")
+	, new File(outputDirectory, "stylesheet.css")
+      );
+
+      Utils.CopyStreamToFile(
+	  (null != sourceStylesheet && sourceStylesheet.exists()) 
+	  ? new FileInputStream ( sourceStylesheet ) 
+	  : (new ResourceLoader()).getResourceStream("defaultstylesheet.css")
+	, new File(outputDirectory, "sourcestylesheet.css")
+      );
+    } catch(FileNotFoundException e) {
+      System.err.println("File not found. ");
+      e.printStackTrace();
+      throw e;
+    }
   }
 
 }
